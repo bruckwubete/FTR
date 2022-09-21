8c64ec1c58781a743c7ed1e80802e6bb6c30a86e
bruckwubete <bruckwendu@gmail.com>
2022-09-20 23:40:19 -0400

    (chore): attempting k transaction option
    Signed-off-by: bruckwubete <bruckwendu@gmail.com>

--- leetcode/best_time_to_buy_and_sell_stock_III.py
+++ leetcode/best_time_to_buy_and_sell_stock_III.py
@@ -1,6 +1,7 @@
 from typing import List
 import sys
 
+
 def maxProfit(self, prices: List[int]) -> int:
     fs = ss = 0
     fb = sb = ~sys.maxsize
@@ -10,3 +11,28 @@ def maxProfit(self, prices: List[int]) -> int:
         sb = max(sb, fs - i)
         ss = max(ss, sb + i)
     return ss
+
+
+def max_profit_2(self, prices: List[int]) -> int:
+    dp = {}
+    profits = []
+
+    def dfs(i, buying):
+        if i > len(prices):
+            return 0
+        if (i, buying) in dp:
+            return dp[(i, buying)]
+
+        if buying:
+            buy = dfs(i + 1, False) - prices[i]
+            cooldown = dfs(i + 1, True)
+            dp[(i, buying)] = max(buy, cooldown)
+        else:
+            sell = dfs(i + 2, True) + prices[i]
+            cooldown = dfs(i + 1, True)
+            dp[(i, buying)] = max(sell, cooldown)
+            profits.append(dp[(i, buying)])
+        return dp[(i, buying)]
+
+    dfs(0, True)
+    return sum(profits.sort(reverse=True)[:2])
