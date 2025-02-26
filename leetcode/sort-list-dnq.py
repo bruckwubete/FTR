704fd68c4f30da3158a4419384d927846a9313e1
bruckwubete <bruckwendu@gmail.com>
2025-02-25 23:31:14 -0500

    (chore): adding my solution to sort-list-dnq.py

---
+++ leetcode/sort-list-dnq.py
@@ -0,0 +1,37 @@
+class Solution:
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # If the head or the entire list is none, return the head
+        if not head or not head.next:
+            return head
+        # Get the middle node
+        mid = self.getMid(head)
+        # Split the list to left and right and sort them
+        left = self.sortList(head)
+        right = self.sortList(mid)
+        # Merge the sorted lists
+        return self.merge(left, right)
+
+    def merge(
+            self, list1: Optional[ListNode], list2: Optional[ListNode]
+    ) -> Optional[ListNode]:
+        dummyHead = ListNode(0)
+        tail = dummyHead
+        while list1 and list2:
+            if list1.val < list2.val:
+                tail.next = list1
+                list1 = list1.next
+            else:
+                tail.next = list2
+                list2 = list2.next
+            tail = tail.next
+        tail.next = list1 if list1 else list2
+        return dummyHead.next
+
+    def getMid(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        midPrev = None
+        while head and head.next:
+            midPrev = head if not midPrev else midPrev.next
+            head = head.next.next
+        mid = midPrev.next
+        midPrev.next = None
+        return mid
